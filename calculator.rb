e0337c7174c8bff6e77cbab9f9855c15f23da40a
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 16:55:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,11 @@
 # Add your variables here
+  first_number = x
+  second_number = y
 
 #SUM
 def sum
-  first_number = 22
-  second_number = 28
+  x = 22
+  y = 28
   puts "If you add #{first_number} and #{second_number}, the sum is #{first_number + second_number}."
 end
 
