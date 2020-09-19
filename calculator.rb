8e6901f8cc833330bb52174a57638bb130d87f8e
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 17:03:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,6 +7,7 @@
   difference = first_number - second_number
   product = first_number * second_number
   quotient = first_number / second_number
+  
 #outputs
   puts "If you add #{first_number} and #{second_number}, the sum is #{sum}."
   puts "If you subtract #{first_number} and #{second_number}, the difference is #{difference}."
