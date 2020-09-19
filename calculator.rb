68a4a02ddac503ea7b582adfdddf08467fe1b68d
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 17:03:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,8 @@
   second_number = 28
   sum = first_number + second_number
   difference = first_number - second_number
-  
+  product = first_number * second_number
+  quotient = first_number 
 #outputs
   puts "If you add #{first_number} and #{second_number}, the sum is #{sum}."
   puts "If you subtract #{first_number} and #{second_number}, the difference is #{difference}."
