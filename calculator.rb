6db095f227f9276ed175759918f9a2338d207fc0
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 17:00:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@
 
   first_number = 22
   second_number = 28
-  sum = first number + second_number
+  sum = first_number + second_number
   puts "If you add #{first_number} and #{second_number}, the sum is #{sum}."
