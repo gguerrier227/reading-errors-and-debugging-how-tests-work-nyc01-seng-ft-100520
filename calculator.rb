0cb22623e0d1f23ccf6a95a87b55f3517ab2906b
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 16:57:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,7 @@
   second_number = 28
 
 #SUM
-def sum (first_number, second_number)
+def sum
   puts "If you add #{first_number} and #{second_number}, the sum is #{first_number + second_number}."
 end
 
