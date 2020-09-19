c8fc149d66ef524c54f37c5ed8457d702175960e
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 17:02:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,11 @@
 # Add your variables here
 
+#variables
   first_number = 22
   second_number = 28
   sum = first_number + second_number
   difference = first_number - second_number
+  
+#outputs
   puts "If you add #{first_number} and #{second_number}, the sum is #{sum}."
   puts "If you subtract #{first_number} and #{second_number}, the difference is #{difference}."
